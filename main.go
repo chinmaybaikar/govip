fee845c50c91c8d410e38ea976b4b4abc0e7c562
chinmaybaikar <chinmaybaikar@gmail.com>
2021-05-10 19:47:04 -0500

    mods

--- main.go
+++ main.go
@@ -27,8 +27,8 @@ import (
 	"github.com/j-keck/arping"
 	log "github.com/sirupsen/logrus"
 	"github.com/vishvananda/netlink"
-	"go.etcd.io/etcd/client/v3/clientv3"
-	"go.etcd.io/etcd/client/v3/clientv3/concurrency"
+	"go.etcd.io/etcd/client/v3/client"
+	"go.etcd.io/etcd/client/v3/concurrency"
 	"go.etcd.io/etcd/client/pkg/transport"
 )
 
